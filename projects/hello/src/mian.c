1faa6276142ec84a5a9140e94df4426d0d5b10ee
lostprobe99 <lostprobe@qq.com>
2023-04-13 22:20:04 +0800

    可运行hello程序

---
+++ projects/hello/src/mian.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char ** argv)
+{
+    printf("\nhello\n");
+    for(int i = 0; i < 10; i++)
+        printf("%d\n", i);
+    return 0;
+}
---
+++ settings.cmake
@@ -0,0 +1,47 @@
+include_guard(GLOBAL)
+
+# set(project_dir "${CMAKE_CURRENT_LIST_DIR}/../../")
+set(project_dir "${CMAKE_CURRENT_LIST_DIR}")
+message("project_dir=${project_dir}")
+file(GLOB project_modules ${project_dir}/projects/*)
+list(
+    APPEND
+        CMAKE_MODULE_PATH
+        ${project_dir}/kernel
+        ${project_dir}/tools/cmake-tool/helpers/
+        ${project_dir}/tools/elfloader-tool/
+        ${project_modules}
+)
+
+include(application_settings)
+
+set(KernelArch "x86" CACHE STRING "" FORCE)
+set(KernelPlatform "pc99" CACHE STRING "" FORCE)
+set(KernelSel4Arch "x86_64" CACHE STRING "" FORCE)
+
+message("KernelArch=${KernelArch} KernelPlatform=${KernelPlatform} KernelSel4Arch=${KernelSel4Arch}")
+include(${project_dir}/kernel/configs/seL4Config.cmake)
+set(CapDLLoaderMaxObjects 20000 CACHE STRING "" FORCE)
+set(KernelRootCNodeSizeBits 16 CACHE STRING "")
+
+# For the tutorials that do initialize the plat support serial printing they still
+# just want to use the kernel debug putchar if it exists
+set(LibSel4PlatSupportUseDebugPutChar true CACHE BOOL "" FORCE)
+
+# Just let the regular abort spin without calling DebugHalt to prevent needless
+# confusing output from the kernel for a tutorial
+set(LibSel4MuslcSysDebugHalt FALSE CACHE BOOL "" FORCE)
+
+# Only configure a single domain for the domain scheduler
+set(KernelNumDomains 1 CACHE STRING "" FORCE)
+
+# We must build the debug kernel because the tutorials rely on seL4_DebugPutChar
+# and they don't initialize a platsupport driver.
+ApplyCommonReleaseVerificationSettings(FALSE FALSE)
+
+# We will attempt to generate a simulation script, so try and generate a simulation
+# compatible configuration
+ApplyCommonSimulationSettings(${KernelSel4Arch})
+if(FORCE_IOMMU)
+    set(KernelIOMMU ON CACHE BOOL "" FORCE)
+endif()
